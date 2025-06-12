22a13299f3d6397b5d4e37d5b86a96fbf1f47bfd
Teguh Susanto <teguh.susanto@hotmail.com>
2025-06-12 08:40:05 +0700

    fixed edit permission

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -992,7 +992,7 @@ class ClientController extends Controller
                     return '<svg fill="#000000" width="80px" height="80px" viewBox="0 0 32 32" id="icon" xmlns="http://www.w3.org/2000/svg"><defs><style>.cls-1{fill:none;}</style></defs><title>no-image</title><path d="M30,3.4141,28.5859,2,2,28.5859,3.4141,30l2-2H26a2.0027,2.0027,0,0,0,2-2V5.4141ZM26,26H7.4141l7.7929-7.793,2.3788,2.3787a2,2,0,0,0,2.8284,0L22,19l4,3.9973Zm0-5.8318-2.5858-2.5859a2,2,0,0,0-2.8284,0L19,19.1682l-2.377-2.3771L26,7.4141Z"/><path d="M6,22V19l5-4.9966,1.3733,1.3733,1.4159-1.416-1.375-1.375a2,2,0,0,0-2.8284,0L6,16.1716V6H22V4H6A2.002,2.002,0,0,0,4,6V22Z"/><rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" class="cls-1" width="32" height="32"/></svg>';
                 }
             })
-            ->addColumn('action', function ($row) {
+            ->addColumn('action', function ($row,Request $request) {
                 $btnEdit = '';
                 $btn = '<div class="btn-group btn-group-vertical" role="group" aria-label="Basic mixed styles example"><button class="btn btn-info btn-sm view-client" data-id="' . $row->id . '">View</button>';
 
@@ -1014,25 +1014,27 @@ class ClientController extends Controller
                                                 data-action="edit" >Open To Delete</button>';
                         }
                     }
-
-                    $edit = $row->clientRequests
-                        ->whereIn('data_status', [1, 3]) // Akan memfilter data_status 2 atau 4
-                        ->sortByDesc('created_at')
-                        ->first();
-                    if (is_null($edit)) {
-                        $btnEdit .= '<button class="btn btn-primary btn-sm view-client-edit" data-id="' . $row->id . '">Request to Edit</button>';
-                    } else {
-                        if (is_null($edit->approved_by)) {
-                            $btnEdit .= '<button class="btn btn-primary btn-sm" disabled>Waiting Response</button>';
+                     if(!$request->filled('master'))
+                     {
+                        $edit = $row->clientRequests
+                            ->whereIn('data_status', [1, 3]) // Akan memfilter data_status 2 atau 4
+                            ->sortByDesc('created_at')
+                            ->first();
+                        if (is_null($edit)) {
+                            $btnEdit .= '<button class="btn btn-primary btn-sm view-client-edit" data-id="' . $row->id . '">Request to Edit</button>';
                         } else {
-                            if ($edit->data_status == 3) {
-                                $btnEdit .= '<a class="btn btn-success btn-sm" href="' . route('v1.client-database.edit', $row->id) . '">Open To Edit</a>';
+                            if (is_null($edit->approved_by)) {
+                                $btnEdit .= '<button class="btn btn-primary btn-sm" disabled>Waiting Response</button>';
                             } else {
-                                $btnEdit .= '<a class="btn btn-success btn-sm" href="' . route('v1.client-database.edit', $row->id) . '">Open To Edit</a>';
+                                if ($edit->data_status == 3) {
+                                    $btnEdit .= '<a class="btn btn-success btn-sm" href="' . route('v1.client-database.edit', $row->id) . '">Open To Edit</a>';
+                                } else {
+                                    $btnEdit .= '<a class="btn btn-success btn-sm" href="' . route('v1.client-database.edit', $row->id) . '">Open To Edit</a>';
+                                }
                             }
                         }
-                    }
 
+                     }
                 }
                 if (auth()->user()->can('delete-client-database')) {
                     $btnDelete .= '<button class="btn btn-danger btn-sm confirm-action" data-id="' . $row->id . '"
