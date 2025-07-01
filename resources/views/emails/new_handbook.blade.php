65b87030ec0e3a8299561ebb9e5629e06e6ad9d0
Teguh Susanto <teguh.susanto@hotmail.com>
2025-07-01 14:42:08 +0700

    enhance change #7

--- resources/views/emails/new_handbook.blade.php
+++ resources/views/emails/new_handbook.blade.php
@@ -5,9 +5,9 @@
 </head>
 
 <body>
-    <h2>Dear <strong>{{ $post['employee_name'] }}</strong>,</h2>
+    <h2>Dear <strong>{{ $post['employee_name'] }}</strong>,</h2><br>
     <p>{{ $post['creator_name'] }} has uploaded a new staff handbook.
-Please <a href="{{ route('v1.login') }}">login</a> to the staff portal to read <a href="{{ route('v1.employee-handbooks.read',['id'=>(int) $post['id']]) }}">the handbook</a>.
+Please login to the staff portal to read the handbook.<br>
 If you have any questions, feel free to reach out to the management. 
 </p>
     <br>
--- resources/views/emails/new_memo.blade.php
+++ resources/views/emails/new_memo.blade.php
@@ -5,9 +5,9 @@
 </head>
 
 <body>
-    <h2>Dear <strong>{{ $post['employee_name'] }}</strong>,</h2>
+    <h2>Dear <strong>{{ $post['employee_name'] }}</strong>,</h2><br>
     <p>{{ $post['creator_name'] }} has issued a new management memo.
-Please <a href="{{ route('v1.login') }}">login</a> to the staff portal to read and acknowledge <a href="{{ route('v1.management-memo.read',['id'=>(int) $post['id']]) }}">the memo</a>.
+Please login to the staff portal to read and acknowledge the memo.<br>
 If you have any questions, feel free to reach out to the management. 
 </p>
     <br>
